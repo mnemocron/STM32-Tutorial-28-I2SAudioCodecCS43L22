6d3e7d7bbffb5057fdf9c7e3229f543dd955532f
Simon Burkhardt <simon.burkhardt.prime@gmail.com>
2019-10-31 13:31:58 +0100

    refactoring + THD issue fixed

---
+++ Debug/Src/subdir.mk
@@ -0,0 +1,40 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/MY_CS43L22.c \
+../Src/main.c \
+../Src/stm32f4xx_hal_msp.c \
+../Src/stm32f4xx_it.c \
+../Src/syscalls.c \
+../Src/system_stm32f4xx.c 
+
+OBJS += \
+./Src/MY_CS43L22.o \
+./Src/main.o \
+./Src/stm32f4xx_hal_msp.o \
+./Src/stm32f4xx_it.o \
+./Src/syscalls.o \
+./Src/system_stm32f4xx.o 
+
+C_DEPS += \
+./Src/MY_CS43L22.d \
+./Src/main.d \
+./Src/stm32f4xx_hal_msp.d \
+./Src/stm32f4xx_it.d \
+./Src/syscalls.d \
+./Src/system_stm32f4xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F407xx -I"C:/Users/simon/Documents/STM32/CubeMX_Tutorial_28-I2S_Audio_Codec/Inc" -I"C:/Users/simon/Documents/STM32/CubeMX_Tutorial_28-I2S_Audio_Codec/Drivers/STM32F4xx_HAL_Driver/Inc" -I"C:/Users/simon/Documents/STM32/CubeMX_Tutorial_28-I2S_Audio_Codec/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy" -I"C:/Users/simon/Documents/STM32/CubeMX_Tutorial_28-I2S_Audio_Codec/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"C:/Users/simon/Documents/STM32/CubeMX_Tutorial_28-I2S_Audio_Codec/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ MDK-ARM/RTE/_CubeMX_Tutorial_28-I2S_Audio_Codec/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'CubeMX_Tutorial_28-I2S_Audio_Codec' 
+ * Target:  'CubeMX_Tutorial_28-I2S_Audio_Codec' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
